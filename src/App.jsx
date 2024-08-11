44755a6d18c804ade06c9e2ce6654a5cbcd5df8b
NayefNemernn <nayefnemernn@gmail.com>
2024-08-11 12:18:18 +0300

    Renaming Login component Form. Deleting Register component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './App.css'
-import Login from './components/Login'
-import Register from './components/Register'
+import Form from './components/Form'
 
-var isLogin = true
+var isLogin = false
 
 function App() {
   return (
     <>
-      {isLogin ? <Login /> : <Register />}
+      <Form areUlogin = {isLogin}/>
     </>
   )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Input from "./Input";
 
-function Login (){
+function Form (props){
     return <>
     <h1 className="register-login">Welcome Back</h1>
     <form action="">
         <Input type ='text' placeholder ='Username'/>
         <Input type ='password' placeholder ='Password'/>
-        <button type="submit">Login</button>
+        {!props.areUlogin && <Input type ='password' placeholder ='Confirm Password'/>}
+        <button type="submit">{props.areUlogin ? 'Login' : 'Register'}</button>
     </form>
     </>
 }
-export default Login;
+export default Form;
